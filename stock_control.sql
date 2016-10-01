bd1b5387a31cad2c05be5452cc8fd593cd959259
isrmic <israelfcfm@outlook.com>
2016-10-01 18:01:05 -0300

    update stock_control.sql

---
+++ stock_control.sql
@@ -0,0 +1,26 @@
+create database Stock_control;
+
+use stock_control;
+
+create table produtos(
+
+ID int(11) not null auto_increment primary key,
+Name varchar(32) not null ,
+Price decimal(9,2) not null,
+Description varchar(120),
+Count_P int(11),
+insertData not null datetime,
+dataModified not null datetime
+
+);
+
+create table user_control(
+
+ID int(11) not null auto_increment primary key,
+
+UserName varchar(16) not null,
+Password char(32) not null,
+UserType int(3)
+);
+
+INSERT INTO user_control (`UserName`, `Password`, `UserType`) values ("admincontrol", "E10ADC3949BA59ABBE56E057F20F883E", 1)
